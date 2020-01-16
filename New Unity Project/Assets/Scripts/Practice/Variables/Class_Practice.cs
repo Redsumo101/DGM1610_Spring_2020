8e795d97eefada96525ef6ea55d1b68f9acd6056
Riley <rileyharper15@gmail.com>
2020-01-15 17:17:24 -0700

    Cookies

---
+++ New Unity Project/Assets/Scripts/Practice/Variables/Class_Practice.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class_Practice : MonoBehaviour
+{
+    public float amount;
+    public float money;
+    public float cost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cookies(amount, money, cost);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Cookies(float amount,float money, float cost)
+    {
+        float total;
+        total = money-cost * amount;
+        print(total);
+    }
+}
