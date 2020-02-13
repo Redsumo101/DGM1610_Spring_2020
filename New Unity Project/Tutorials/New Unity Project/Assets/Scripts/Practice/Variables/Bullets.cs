002dfa2adff10e901427fa0c869722fb52568abb
Riley <rileyharper15@gmail.com>
2020-02-13 09:21:44 -0700

    File fix

---
+++ New Unity Project/Tutorials/New Unity Project/Assets/Scripts/Practice/Variables/Bullets.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+   public float bullets;
+   public float shots_taken;
+   public float reloads;
+      
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ammo(bullets, shots_taken, reloads);
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Ammo(float bullets, float shots_taken, float reloads)
+    {
+       
+
+        float total;
+        total = bullets - shots_taken + reloads;
+        if (bullets < 0)
+        {
+            Debug.Log(bullets + reloads);
+        }
+        print(total);
+        }
+}
---
+++ New Unity Project/Tutorials/New Unity Project/Assets/Scripts/Practice/Variables/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Game Score: " + score);
+    }
+
+    public static void AddPoints(int pointsToAdd)
+    {
+        score += pointsToAdd;
+    }
+
+}
