a8e640ceb4c522ea418320f812a014b99df5781e
Riley <rileyharper15@gmail.com>
2020-03-11 17:34:12 -0600

    burn

--- New Unity Project/Tutorials/New Unity Project/Assets/Scripts/Class/Variables/FireDamage.cs
+++ New Unity Project/Tutorials/New Unity Project/Assets/Scripts/Class/Variables/FireDamage.cs
@@ -7,10 +7,14 @@ public class FireDamage : TakeDamage
     public int damage = 5;
     public float burnMin = 1;
     public float burnMax = 3;
+    private EnemyHealth burnEffect;
+    public int burnTime = 3;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+        burnEffect = GameObject.Find("Enemy").GetComponent<EnemyHealth>();
     }
 
     // Update is called once per frame
@@ -21,13 +25,18 @@ public class FireDamage : TakeDamage
     }
     public void OnTriggerEnter(Collider other)
     {
+        while (Time.deltaTime *burnTime >= 0) { 
+            currentHealth + burnEffect
+}
         if (other.gameObject.CompareTag("Enemy")) { 
+
+           
+    
+
+
 
         }
     }
-    void BurnEnemy()
-    {
-
-    }
+    
 
 }
---
+++ New Unity Project/Tutorials/New Unity Project/Assets/Scripts/Class/Variables/Loops.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    public int numEnemies = 5;
+    public int cupsInTheSink = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i <= numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number:" + i);
+        }
+        while(cupsInTheSink > 0)
+        {
+            Debug.Log("I've washed a cup");
+            cupsInTheSink--;
+        }
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello World");
+        } while (shouldContinue == true);
+        string[] strings = new string[3];
+        strings[0] = "First string";
+        strings[1] = "Second string";
+        strings[2] = "Third string";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
