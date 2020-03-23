fe16902f76ef07dd33780189d774fadd00d3be6e
Riley <rileyharper15@gmail.com>
2020-03-23 16:24:09 -0600

    StatusEffectManager
    I moved the EnemyHealth fire stuff to a status effect manager to make it better and easier to add other effects

--- New Unity Project/Tutorials/New Unity Project/Assets/Scripts/Class/Variables/EnemyHealth.cs
+++ New Unity Project/Tutorials/New Unity Project/Assets/Scripts/Class/Variables/EnemyHealth.cs
@@ -8,7 +8,7 @@ public class EnemyHealth : MonoBehaviour
     public int maxHealth = 20;
     public Transform spawnPoint;
     public float points = 10;
-    public int time;
+   
   
     // Start is called before the first frame update
     void Start()
@@ -29,38 +29,6 @@ public class EnemyHealth : MonoBehaviour
             currentHealth = maxHealth;
         }
     }
-    private void OnCollisionEnter(Collision other)
-    {
-       
-        if (other.gameObject.CompareTag("FireBullet"))
-        {
-            StartCoroutine(Burn());
-
-        }
-    }
-    // Update is called once per frame
-   IEnumerator Burn()
-    {
-        print("Burn");
-        GetComponent<Renderer>().material.color = Color.red;
-        
-        yield return new WaitForSeconds(time);
-
-
-      /* 
-        while (time > 0)
-        {
-            int ember = Random.Range(1, 2);
-            Debug.Log(currentHealth - ember);
-        }
-        */
-
-
-
-
-
-
-    }
-   
+     
 
 }
--- New Unity Project/Tutorials/New Unity Project/Assets/Scripts/PersonalPractice/Element/StatusEffectManager.cs
+++ New Unity Project/Tutorials/New Unity Project/Assets/Scripts/PersonalPractice/Element/StatusEffectManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class StatusEffectManager : MonoBehaviour
 {
-    private Health healthScript;
+    private EnemyHealth healthScript;
+    public List<int> burnTickTimers = new List<int>();
+    public int time;
     void Start()
     {
-        healthScript = GetComponent<Health>();
+        healthScript = GetComponent<EnemyHealth>();
 
     }
 
@@ -16,4 +18,21 @@ public class StatusEffectManager : MonoBehaviour
     {
         
     }
+    private void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.CompareTag("FireBullet"))
+        {
+            StartCoroutine(Burn());
+
+        }
+    }
+    IEnumerator Burn()
+    {
+        print("Burn");
+        GetComponent<Renderer>().material.color = Color.red;
+
+        yield return new WaitForSeconds(time);
+    }
+
 }
