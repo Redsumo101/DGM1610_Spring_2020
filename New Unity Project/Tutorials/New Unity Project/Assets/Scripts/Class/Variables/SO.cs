7f9cf066cff4bf8882710d8aca9f79025f8e118b
Riley <rileyharper15@gmail.com>
2020-04-14 08:58:55 -0600

    WeaponTypePickup

---
+++ New Unity Project/Tutorials/New Unity Project/Assets/Scripts/Class/Variables/SO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New SO", menuName ="Scriptable Object")]
+
+public class SO : ScriptableObject
+{
+    public string fullName;
+
+    public string description;
+
+    public GameObject model;
+
+    public Color color;
+}
