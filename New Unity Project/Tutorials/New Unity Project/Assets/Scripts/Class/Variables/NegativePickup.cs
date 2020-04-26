36433d86e1163ef7c7dfe3f684374d851c78cc70
Riley <rileyharper15@gmail.com>
2020-04-26 11:21:15 -0600

    Pickups

---
+++ New Unity Project/Tutorials/New Unity Project/Assets/Scripts/Class/Variables/NegativePickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NegativePickup : PickUp
+{
+    public int pointsToSub;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.SubPoints(pointsToSub);
+            Destroy(gameObject);
+        }
+
+    }
+}
